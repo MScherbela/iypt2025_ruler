3ea65f5f8ce37c455246b84deba998d35f431f21
David Mayr <mayr-david@gmx.at>
2025-06-18 21:26:17 +0200

    Rework of arc length theory

--- ruler_velocity.py
+++ ruler_velocity.py
@@ -1,13 +1,11 @@
-# %%
 import math
 import numpy as np
 import csv
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-from ruler_equations import get_touch_angle, return_coeffs, evaluate_polynomial, arc_length, y_end_optimum
+from ruler_equations import get_touch_angle,return_coeffs, evaluate_polynomial, arc_length, y_end_optimum
 from scipy.integrate import quad
 from scipy.optimize import root_scalar
-
 
 class CriticalDistance:
     def __init__(self):
@@ -16,134 +14,151 @@ class CriticalDistance:
 
     def __call__(self, t, y):
         x = float(y[0])
-        return x - 0.17  # = length L2
+        return x - (L1+r_ball) # = length L2
 
-
-def x_dotdot(x_ball, xdot):  #
+def x_dotdot(x_ball, xdot):#
     global arc
 
-    # side of ruler touches projectile
-    if arc == False:
-        touch_angle, x_end, y_end, slope_end = get_touch_angle(EI, x_load, force, x_ball, r_ball, load)
-        coeffs_right = return_coeffs(x_end, y_end, load, x_load, slope_end)[4:]
-        force_right = np.abs(evaluate_polynomial(coeffs_right, x_end, 3) * E_modulus * I_moment_of_inertia)
+    #side of ruler touches projectile
+    if(arc == False):
+        touch_angle, x_end, y_end, slope_end = get_touch_angle(EI, x_load, force, x_ball, r_ball,load)
+        coeffs_right = return_coeffs(x_end, y_end,load, x_load, slope_end)[4:]
+        force_right = evaluate_polynomial(coeffs_right,x_end, 3) * E_modulus * I_moment_of_inertia
         theta = np.abs(touch_angle)
 
-        # veryfying whether just the tip of the ruler touches the projectile
-        if arc_length(0.0, x_end, EI, x_load, load, x_end, y_end, slope_end) - L1 > 0.0:
+        #veryfying whether just the tip of the ruler touches the projectile
+        if(arc_length(0.0, x_end,EI, x_load,load, x_end, y_end, slope_end)-L1>0.0):
             arc = True
 
-    # tip of ruler touches projectile
-    if arc == True:  # = if(arc)
-        x_end, y_end, touch_angle, coeffs = y_end_optimum(x_load, load, r_ball, x_ball, EI, L1)
+    #tip of ruler touches projectile
+    if(arc == True): # = if(arc)
+        x_end, y_end, touch_angle, coeffs =y_end_optimum(x_load, load, r_ball, x_ball, EI, L1)
         coeffs_right = coeffs[4:]
-        force_right = np.abs(evaluate_polynomial(coeffs_right, x_end, 3) * E_modulus * I_moment_of_inertia)
+        force_right = evaluate_polynomial(coeffs_right,x_end, 3) * E_modulus * I_moment_of_inertia
         theta = np.abs(touch_angle)
 
-        # ruler is out of reach
-        if np.isnan(y_end):
-            return -(mu_rolling * m * g) / m * (xdot > 0.0) + 0.0
+        #ruler is out of reach or an error occured
+        if np.isnan(y_end): return 0.0      
 
-    k = (r_ball - y_end) / (x_ball - x_end)
-    phi = np.abs(np.arctan(k))
-    gamma = np.pi / 2 - (phi + theta)
+    force_right = force_right * np.cos(tilt_radian) # tilt in the system
 
-    if xdot == 0.0:
-        mu = mu_static
-    else:
-        mu = mu_kinetic
+    k=(r_ball-y_end)/(x_ball-x_end)
+    phi=np.abs(np.arctan(k))
+    gamma = np.pi/2-(phi+theta)
 
-    Fg = m * g
-    Fn = force_right * np.cos(theta)
-    F = Fn * np.cos(gamma)
+    if(xdot == 0.0): mu = mu_static
+    else: mu = mu_kinetic
+
+    Fgy = np.abs(m*g*np.cos(tilt_radian)) # = Fg for no tilt
+    Fgx = -m*g * np.sin(tilt_radian) # = 0 for no tilt
+
+    Fs = force_right#*np.cos(theta)
+    F = Fs*np.cos(gamma)
     Fx = F * np.cos(phi)
     Fy = F * np.sin(phi)
 
-    FR1 = mu * (Fy) * np.cos(theta)
-    FR2 = mu * (Fy + Fg)
+    FR1 = -mu *(Fy)*np.cos(theta)
+    FR2 = -mu * (Fy+Fgy)
 
-    F_net = Fx - FR1 - FR2
-    if F_net >= 0.0:
-        return F_net / m
+    F_net = (Fx + FR1 + FR2 + Fgx)
+    if(F_net >=0.0): return F_net/m
+    if(F_net <0.0):
+        if(xdot > 0): return F_net/m
+        else: return 0.0
 
-    if F_net < 0.0:
-        if xdot > 0:
-            return F_net / m
-        else:
-            return 0.0
-
-
-def dydt(t, y):
-    return np.array([y[1], x_dotdot(y[0], y[1])])
-
+def dydt(t,y):
+    return np.array([y[1],x_dotdot(y[0], y[1])])
 
 param_values = []
 x_dot_values = []
 
-param_i = 0.0
-param_f = 0.4
-n = 100
-step_size = (param_f - param_i) / n
+param_i= 0.0
+param_f= 0.35
+n= 30
+step_size =(param_f-param_i)/n
 
-for i in np.arange(param_i, param_f, step_size):
+for i in np.arange (param_i, param_f,step_size):
     m_o = i
     print(i)
 
-    ##############################################################################################################################
+##############################################################################################################################
 
     arc = False
 
-    # properties of the ruler
+    #properties of the ruler
     L1 = 0.13
-    L2 = 0.17
+    L2 = L1 + 0.04
     tilt = 0
-    phi = tilt * np.pi / 180
+    tilt_radian = tilt * np.pi / 180
     ruler_width = 0.03
-    ruler_height = 0.003
-    E_modulus = 2.1 * 10**9
+    ruler_height = 0.0015
+    E_modulus = 2.3*10**9
     I_moment_of_inertia = ruler_width * ruler_height**3 / 12
     EI = E_modulus * I_moment_of_inertia
 
-    # properties of the projectile
-    x_ball = 0.133
+    #properties of the projectile
+    x_ball = L1+0.003 
     m = 0.05
     r_ball = 0.01
     w = r_ball * 2
     A = 2 * r_ball * w
-    V = math.pow(r_ball, 2) * math.pi * w
+    V = r_ball**2 * np.pi * w
     Cd = 1
     rho = 1.293
 
     # Friction coefficients
     mu_static = 0.15
-    mu_kinetic = 0.10
+    mu_kinetic = 0.08
     mu_rolling = 0.01
 
-    # force properties
-    """m_o = 0.25"""
-    g = 9.81
-    x_load = 0.08
-    force = m_o * 9.81
+    #force properties
+    '''m_o = 0.25'''
+    g= 9.81
+    x_load = L1 - 0.05
+    force = m_o *g*np.cos(tilt_radian) # = m_o * g for no tilt
     load = force / EI
 
-    ##############################################################################################################################
+##############################################################################################################################
 
     y_0 = [x_ball, 0.0]
     t0 = 0.0
     t1 = 0.5
     should_continue = False
     try:
-        sol = solve_ivp(dydt, t_span=[t0, t1], y0=y_0, events=CriticalDistance())
+        sol = solve_ivp(dydt, t_span=[t0, t1], y0=y_0, events=CriticalDistance(), rtol=1e-3, atol=1e-6)
 
         if sol.status != 1:
             print("took too long")
 
         x_dot_max = np.max(sol.y[1])
         param_values.append(i)
-        x_dot_values.append(x_dot_max)
-    except:
-        continue
+        x_dot_values.append(x_dot_max) 
+    except: continue
 
-plt.plot(param_values, x_dot_values)
+###############################################################################################################################
+
+threshold = 0.15
+
+# Division into segments
+segments = []
+current_x = [param_values[0]]
+current_y = [x_dot_values[0]]
+
+for i in range(1, len(x_dot_values)):
+    if abs(x_dot_values[i] - x_dot_values[i-1]) > threshold:
+        segments.append((current_x, current_y))
+        current_x = [param_values[i]]
+        current_y = [x_dot_values[i]]
+    else:
+        current_x.append(param_values[i])
+        current_y.append(x_dot_values[i])
+
+segments.append((current_x, current_y))  # last segment
+
+# Segment plotting
+for x_seg, y_seg in segments:
+    plt.plot(x_seg, y_seg, color = "blue")
+
+plt.xlabel("Parameter")
+plt.ylabel(r"$x_{dot}$")
 plt.show()
-# plt.close()
--- ruler_velocity_simple.py
+++ ruler_velocity_simple.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.patches import Circle
 
-v_ball_max_values = []
-parameters = []
+x_dot_values = []
+param_values = []
 
-for i in np.arange (0.0, 4, 0.01):
+for i in np.arange (0.0,1.5, 0.01):
     print(i)
     m_o = i
-    parameters.append(i)
+    param_values.append(i)
 
     # properties of the ruler
     L1 = 0.13
@@ -37,12 +37,12 @@ for i in np.arange (0.0, 4, 0.01):
     rho = 1.293
 
     # Friction coefficients
-    mu_static = 0.0
-    mu_kinetic = 0.0
+    mu_static = 0.10
+    mu_kinetic = 0.05
     mu_rolling = 0.0
 
     # force properties
-    '''m_o = 0.25'''
+    #m_o = 0.25
     g = 9.81
     x_load = 0.08
     force = m_o * 9.81
@@ -51,6 +51,7 @@ for i in np.arange (0.0, 4, 0.01):
     # 1) Get touch angle => get initial coefficients, y_end
     touch_angle, x_end, y_end, slope_end = get_touch_angle(EI, x_load, force, x_ball, r_ball,load)
     coeffs_right = return_coeffs( x_end, y_end, load, x_load, slope_end)[4:]
+
 
     # 2) Integrate ODE with euler method => save coefficients at every step
     # Use open end equations (y''(right) = 0) for the beam
@@ -63,25 +64,24 @@ for i in np.arange (0.0, 4, 0.01):
     ball_positions = []
     E_pots = []
     time = []
-
+    velocity = []
+    
     for i in range(n_steps):
-        touch_angle, x_end, y_end, slope_end = get_touch_angle(EI, x_load, force, x_ball, r_ball,load)
-        #if(x_ball < 0.14):y_end = np.sqrt(r_ball**2-(x_end-x_ball)**2)+r_ball
-        #else: y_end = 0.0
+        if(x_ball <L1+r_ball): y_end = np.sqrt(r_ball**2-(x_end-x_ball)**2)+r_ball
+        else: y_end = 0.0
 
         theta = touch_angle
         phi = np.abs(np.arctan((r_ball-y_end)/(x_ball - x_end)))
         gamma = np.pi/2 - (theta+phi)
-
         coeffs = return_coeffs( x_end, y_end, load, x_load, slope_end)[4:]
 
-        Force_end_y = np.abs(evaluate_polynomial(coeffs_right, x_end, 3) * E_modulus * I_moment_of_inertia)
+        Force_end_y = evaluate_polynomial(coeffs_right, x_end, 3) * E_modulus * I_moment_of_inertia
         if(x_ball > 0.14): 
             Force_end_y = 0.0
             coeffs = ([0.0,0.0,0.0,0.0])
             theta = 0.0
         
-        Force_end_n = Force_end_y #* np.cos(touch_angle)
+        Force_end_n = Force_end_y * np.cos(touch_angle)
         Force_ball_proj = Force_end_n * np.cos(gamma)
         Force_ball_x = Force_ball_proj * np.cos(phi)
         Force_ball_y = Force_ball_proj * np.sin(phi)
@@ -91,77 +91,85 @@ for i in np.arange (0.0, 4, 0.01):
         else: mu = mu_rolling
         FR1 = -mu *Force_ball_y*np.cos(theta)
         FR2 = -mu *(Force_ball_y+m_ball*g)
+        Fd = -0.5 * Cd * A * v_ball**2
 
-        F = Force_ball_x + FR1 + FR2
-
-        v_ball += F * dt / m_ball
-        if v_ball > v_ball_max: v_ball_max = v_ball
-        x_ball += v_ball * dt
+        F = Force_ball_x + FR1 + FR2 +Fd
+        a_ball = F / m_ball
+        if(F>0.0): v_ball += a_ball* dt 
+        elif(v_ball > 0.0): v_ball += a_ball* dt 
+        else: v_ball += 0.0
 
         E_left = get_bending_energy(coeffs_right, 0, x_load, EI)
         E_right = get_bending_energy(coeffs_right, x_load, x_end, EI)
         E_load = force * evaluate_polynomial(coeffs_right, x_load, 0)
         E = E_left + E_right + E_load
 
+        if v_ball > v_ball_max: v_ball_max = v_ball
         all_coeffs.append(coeffs)
         ball_positions.append(x_ball)
         E_pots.append(E)
+        velocity.append(v_ball)
         time.append(n_current*dt)
         n_current += 1
+
         if(x_ball > 0.17): break
 
     all_coeffs = np.array(all_coeffs)
     ball_positions = np.array(ball_positions)
     E_pots = np.array(E_pots)
     time = np.array(time)
-    v_ball_max_values.append(v_ball_max)
+    velocity=np.array(velocity)
+    x_dot_values.append(v_ball_max)
 
-parameters = np.array(parameters)
-v_ball_max_values = np.array(v_ball_max_values)
+param_values = np.array(param_values)
+x_dot_values = np.array(x_dot_values)
 
-# 3) Animation of the beam using the dumped coefficients
-
-plt.plot(parameters, v_ball_max_values)
+# 3) plotting relevant data
+plt.plot(time, velocity)
+plt.plot(time, E_pots)
+plt.plot(time, ball_positions)
 plt.show()
 
-
-# Plotbereich
+# 4) Animation of the beam using the dumped coefficients
+# Plotting  rang
 fig, ax = plt.subplots()
 ax.set_xlim(0, 0.2)
 ax.set_ylim(-0.01, 0.06)
 ax.set_aspect('equal')
 
-# Initiale Balkenlinie und Ballobjekt
+# Initial conditions
 beam_line, = ax.plot([], [], lw=2)
 ball_patch = Circle((0, 0), r_ball, color='red')
 ax.add_patch(ball_patch)
 
-# x-Werte für Auswertung des Balkens
+# x-values for determination y(x)
 x_vals = np.linspace(0, x_end, 200)
 
-# Initialisierung der leeren Szene
+# Initialisation
 def init():
     beam_line.set_data([], [])
     ball_patch.center = (0, 0)
     return beam_line, ball_patch
 
-# Update-Funktion für jedes Frame
+# Update function
 def update(i):
+    # Beam modelling
     coeffs = all_coeffs[i]
     y_vals = evaluate_polynomial(coeffs, x_vals, 0)
-
     beam_line.set_data(x_vals, y_vals)
 
-    # Ball-Position berechnen
+    # Position of the Ball
     x = ball_positions[i]
-    # y aus Kreiskontakt zur Beamfläche
-    y_beam = evaluate_polynomial(coeffs, x, 0)
-    '''y = y_beam + np.sqrt(r_ball**2 - (x_vals[np.abs(x_vals - x).argmin()] - x)**2)'''
-
     ball_patch.center = (x, r_ball)
+
     return beam_line, ball_patch
 
-# Erstelle Animation
+# Animation
 ani = animation.FuncAnimation(fig, update, frames=len(time),
                               init_func=init, blit=True, interval=100, repeat = True)
+plt.show()
+
+# 5) Parameter plotting
+
+plt.plot(param_values,x_dot_values)
 plt.show()
